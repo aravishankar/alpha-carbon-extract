5e3e0d593a2d415ee7a5c45dbb5adbb114c1decb
Aryan <aryanravishankar02@gmail.com>
2020-06-08 13:16:48 -0400

    Commit

--- prog.py
+++ prog.py
@@ -5,7 +5,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.metrics import *
 from matplotlib.pyplot import *
-from matplotlib.cm import *
+import matplotlib.cm as cm
+from matplotlib.colorbar import Colorbar
 
 parser = PDBParser()
 io=PDBIO()
@@ -51,6 +52,10 @@ def getCoordinates():
         ydata.append(coord[1])
         zdata.append(coord[2])
 
+#Gets cutoff distance from user        
+def getCutoff():
+    return int(input("Cutoff Distance: "))
+
 #Generates a C-Alpha Trace by plotting and connecting data
 def cAlpha(xdata, ydata, zdata):
     fig = plt.figure()
@@ -60,14 +65,15 @@ def cAlpha(xdata, ydata, zdata):
     ax.scatter3D(xdata, ydata, zdata, s=1, c="r", cmap='hsv')
     plt.show()
 
-# def cAlphaColor(xdata, ydata, zdata):
-#     fig = plt.figure()
-#     ax = plt.axes(projection='3d')
+#Generates a C-Alpha Trace, colored by number of CA within cutoff distance for each CA
+def cAlphaColor(xdata, ydata, zdata, cutOff):
+    fig = plt.figure()
+    ax = plt.axes(projection='3d')
 
-#     ax.plot3D(xdata, ydata, zdata)
-#     p = ax.scatter3D(xdata, ydata, zdata, s=1, c="r", cmap='hsv')
-#     fig.colorbar(p)
-#     plt.show()
+    ax.plot3D(xdata, ydata, zdata, c="black", alpha=0.5)
+    p = ax.scatter3D(xdata, ydata, zdata, s=5, c=withinDistance(cutOff), cmap='hsv')
+    fig.colorbar(p)
+    plt.show()
 
 #Generates a Pairwise Distance Matrix Plot    
 def pdMatrix(coords):
@@ -91,21 +97,25 @@ def withinDistance(cutOff):
         
     return np.array(output)
 
-# def distanceColor():
-#     cutOff = int(input("Cutoff Distance:"))
+# def distanceColor(cutOff):
+    
 #     fig = plt.figure()
 #     ax = plt.axes()
-#     ax.plot(withinDistance(cutOff))
-#     fig.colorbar(cm.ScalarMappable(norm=norm, cmap=cmap), ax=ax)
+    
+#     ax.axhline(y=0, xmin=0, xmax=len(coords), c=withinDistance(cutOff), cmap='hsv')
+    
+#     # ax.plot(labels, withinDistance(cutOff))
+#     # fig.colorbar(cm.ScalarMappable(cmap='autumn'), ax=ax)    
     
 #     plt.show()
 
 #Main Procedure
 getAlphaCarbons(fileName)
 getCoordinates()
+cutOff = getCutoff()
 
 # cAlpha(xdata, ydata, zdata)
-# # cAlphaColor(xdata, ydata, zdata)
+# cAlphaColor(xdata, ydata, zdata, cutOff)
 # pdMatrix(coords)
-# print(withinDistance(5))
-# distanceColor()
+# print(withinDistance(cutOff))
+# distanceColor(cutOff)
